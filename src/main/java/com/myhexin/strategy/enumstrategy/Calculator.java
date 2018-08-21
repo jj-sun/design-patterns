5500f29f0b615e90cbaac43606d922497a7a5d35
sunjie <sunjie@myhexin.com>
2018-08-21 19:55:58 +0800

    增加注释

--- src/main/java/com/myhexin/strategy/enumstrategy/Calculator.java
+++ src/main/java/com/myhexin/strategy/enumstrategy/Calculator.java
@@ -1,6 +1,10 @@
 package com.myhexin.strategy.enumstrategy;
 
 public enum Calculator {
+    /**
+     * 这里，每一个枚举实例都是  public static final 类型的,扩展性受到了一定的约束。
+     *
+     */
     ADD("+") {
         @Override
         public int exec(int a, int b) {
@@ -15,7 +19,10 @@ public enum Calculator {
     };
     private String value;
 
-
+    /**
+     * 枚举类的构造方法是private的，只能是private的
+     * @param value
+     */
     Calculator(String value) {
         this.value = value;
     }
